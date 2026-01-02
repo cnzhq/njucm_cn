f25defa7aeea9b992ddc370cc556c8b1442eb3cb
cnzhq <cnzhq@outlook.com>
2026-01-02 22:09:23 +0800

    bottom change

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            点此开始
           </Link>
         </div>
       </div>
